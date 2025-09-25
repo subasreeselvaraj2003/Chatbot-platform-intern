ce5d219575783030a00563599542f7ba72cc3626
subasreeselvaraj2003 <subasreeselvaraj2003@gmail.com>
2025-09-25 18:50:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -164,6 +164,9 @@ def chat(project_id):
 
 # -------------------- Run App --------------------
 if __name__ == '__main__':
+    import os
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+        port=int(os.environ.get("PORT",10000))
+    app.run(host="0.0.0.0",port=port,debug=True)
+
